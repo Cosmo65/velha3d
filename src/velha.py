165e7c178ef3ff9d7a19901e9fb35792c14f5e77
jessaraujo <jess-araujo@live.com>
2014-07-01 01:18:08 +0000

    Resposta do Desafio

--- src/velha.py
+++ src/velha.py
@@ -1,17 +1,15 @@
 from glow import *
-_gs=glow('main')
+_gs = glow('main')
 cena = canvas()
 cena.width = 400
 cena.height = 400
-#box(color=color.blue)
-#box(size=(2,2,2),opacity=0.2)
-#sphere(pos=(2,2,2), color =color.red)
+TAM = (-1, 0, 1)
+TABULEIRO = []
+PECAS = []
 print("oi")
-pecas =[(box,color.blue), (sphere, color.red)] * 5
-for linha in range(3):
-   for coluna in range(3):
-       for camada in range(3):
-        box(pos=(coluna*3,linha *3,camada *3), size=(2,2,2), opacity=0.2)
-#for linha in range(3):
-   #for coluna in range(3):
-       #box(pos=(coluna*3,0,0), size=(2,2,2), opacity=0.2)
+pecas = [(box, color.blue), (sphere, color.red)] * 5
+TABULEIRO = [box(pos=(coluna*3, linha*3, 0), size=(2, 2, 2), opacity=0.2)
+             for coluna in TAM for linha in TAM]
+cor = [(color.red), (color.blue)] * 5
+PECAS = [pecas.pop()[0](pos=(coluna*3, linha*3, 0), color=cor.pop(),  size=(1, 1, 1), opacity=0.6)
+             for coluna in TAM for linha in TAM]
