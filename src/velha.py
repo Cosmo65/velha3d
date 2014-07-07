ee19b1bf463c87fe9b55f72da150e97a0acd4194
jessaraujo <jess-araujo@live.com>
2014-07-07 20:15:23 +0000

    Com classes 'casa' e 'peca'

--- src/velha.py
+++ src/velha.py
@@ -7,13 +7,28 @@ TAM = (-1, 0, 1)
 TABULEIRO = []
 PECAS = []
 print("oi")
-pecas =[(box, color.blue), (sphere, color.red)] * 15
-TABULEIRO = [box(pos=(coluna*3, linha*3, camada*3), size=(2, 2, 2), opacity=0.2)
+pecas =[box, sphere] * 14
+cores = [color.red, color.blue] * 14
+class Casa:
+    def __init__(self, x, y, z):
+        self.e_casa = box(pos=(x*3, y*3, z*3), size=(2, 2, 2), opacity=0.2)
+        self.peca = None
+    def recebe(self, algo3d):
+        self.peca = algo3d
+        return algo3d
+
+class Peca:
+    def __init__(self, tipo_peca, x, y, z, cor):
+            self.e_peca = tipo_peca(pos=(x*3, y*3, z*3), color=cor,  size=(1, 1, 1), opacity=0.6)
+
+TABULEIRO = [Casa(coluna, linha, camada)
              for coluna in TAM
              for linha in TAM
              for camada in TAM]
-cor = [(color.red), (color.blue)] * 15
-PECAS = [pecas.pop()[0](pos=(coluna*3, linha*3, camada*3), color=cor.pop(),  size=(1, 1, 1), opacity=0.6)
+
+PECAS = [Peca(pecas.pop(), coluna, linha, camada, cores.pop())
              for coluna in TAM
              for linha in TAM
              for camada in TAM]
+
+PECASCOLOCADAS = [casa.recebe(peca) for casa, peca in zip(TABULEIRO, PECAS)]
