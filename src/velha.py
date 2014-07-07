78917bc93439852d1c0f7b5c94cc7be237f98cfc
jessaraujo <jess-araujo@live.com>
2014-07-07 18:39:01 +0000

    Em 3D com pecas coloridas e camadas.

--- src/velha.py
+++ src/velha.py
@@ -7,9 +7,13 @@ TAM = (-1, 0, 1)
 TABULEIRO = []
 PECAS = []
 print("oi")
-pecas = [(box, color.blue), (sphere, color.red)] * 5
-TABULEIRO = [box(pos=(coluna*3, linha*3, 0), size=(2, 2, 2), opacity=0.2)
-             for coluna in TAM for linha in TAM]
-cor = [(color.red), (color.blue)] * 5
-PECAS = [pecas.pop()[0](pos=(coluna*3, linha*3, 0), color=cor.pop(),  size=(1, 1, 1), opacity=0.6)
-             for coluna in TAM for linha in TAM]
+pecas =[(box, color.blue), (sphere, color.red)] * 15
+TABULEIRO = [box(pos=(coluna*3, linha*3, camada*3), size=(2, 2, 2), opacity=0.2)
+             for coluna in TAM
+             for linha in TAM
+             for camada in TAM]
+cor = [(color.red), (color.blue)] * 15
+PECAS = [pecas.pop()[0](pos=(coluna*3, linha*3, camada*3), color=cor.pop(),  size=(1, 1, 1), opacity=0.6)
+             for coluna in TAM
+             for linha in TAM
+             for camada in TAM]
